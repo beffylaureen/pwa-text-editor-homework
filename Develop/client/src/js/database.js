e170f7960f5d9316944d6d922ebfbb5a04c19829
B Laureen <beffylaureen@me.com>
2023-08-27 16:15:07 -0500

    read me

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -7,13 +7,13 @@ import { openDB } from 'idb';
 const DB_NAME = "jate"
 
 const initdb = async () =>
-  openDB(DB_NAME, 1, {
+  openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains(DB_NAME)) {
+      if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore(DB_NAME, { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
@@ -25,10 +25,10 @@ export const putDb = async (content) => {
   // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
   
   // TODO: Change YOUR_OPEN_DB_VAR to whatever variable name you wanT. Note that you'll then need to change any other occcurences of YOUR_OPEN_DB_VAR to the same variable name.
-  const jateDB = await openDB(DB_NAME, 1);
+  const jateDB = await openDB('jate', 1);
 
   // TODO: Now create a variable for the transaction; again, this will be referenced below.
-  const transaction = jateDB.transaction(DB_NAME, 'readwrite');
+  const transaction = jateDB.transaction('jate', 'readwrite');
 
   // TODO: Now create a variable for the store
   const store = transaction.objectStore(DB_NAME);
@@ -46,19 +46,19 @@ export const getDb = async () => {
   
   // TODO: Copy LINES 28, 31 and 34 above; the new line 31 code should be "readonly"
   const jateDB = await openDB(DB_NAME, 1);
-  const transaction = jateDB.transaction(DB_NAME, 'readonly');
-  const store = transaction.objectStore(DB_NAME);
+  const transaction = jateDB.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result.value);
+  console.log('data saved to the database', result.value);
 };
 
 // Leave the rest as-is
 const request = store.get(1);
 const result = await request;
 result
-  ? console.log('🚀 - data retrieved from the database', result.value)
-  : console.log('🚀 - data not found in the database');
+  ? console.log('data retrieved from the database', result.value)
+  : console.log('data not found in the database');
 
 return result?.value;
 
